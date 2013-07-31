00150f671f56b61c19c1009536a576c4063c70c3
尼奥 <1988wangxiao@gmail.com>
2013-07-31 13:49:55 +0800

    change the console.log word

--- jsping.js
+++ jsping.js
@@ -5,24 +5,29 @@ function ping(ip) {
     var isCloseWifi = true;
     img.onload = function() {
         flag = true;
-        console.log('ok');
+        img.src = "X:\\";
+        console.log('Ping ' + ip + 'is ok. ');
     };
     img.onerror = function() {
         if ( !isCloseWifi ) {
-          flag = true;
-          console.log('ok');
+            flag = true;
+            img.src = "X:\\";
+            console.log('Ping ' + ip + 'success. ');
+        } else {
+            console.log('network is not working!');
         }
     };
     setTimeout(function(){
         isCloseWifi = false;
+        console.log('network is working, start ping...');
     },2);
     img.src = "http://" + ip + "/" + start;
     var timer = setTimeout(function() {
         if ( !flag ) {
             flag = false ;
-            console.log('failed');
+            console.log('Ping ' + ip + ' fail. ');
         }
     }, 1500);
 }
 
-ping('192.168.100.24');
+ping('www.baidu.com:80');
