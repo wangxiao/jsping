73393723f490f62b23ce3a6c7e6edf07dd3cc098
尼奥 <1988wangxiao@gmail.com>
2013-07-31 13:59:45 +0800

    fix bug

--- jsping.js
+++ jsping.js
@@ -1,29 +1,43 @@
 function ping(ip) {
+
     var img = new Image();
     var start = new Date().getTime();
     var flag = false;
     var isCloseWifi = true;
+    var hasFinish = false;
+
     img.onload = function() {
-        flag = true;
-        img.src = "X:\\";
-        console.log('Ping ' + ip + 'is ok. ');
-    };
-    img.onerror = function() {
-        if ( !isCloseWifi ) {
+        if ( !hasFinish ) {
             flag = true;
-            img.src = "X:\\";
-            console.log('Ping ' + ip + 'success. ');
-        } else {
-            console.log('network is not working!');
+            hasFinish = true;
+            img.src = 'X:\\';
+            console.log('Ping ' + ip + ' success. ');
         }
     };
+
+    img.onerror = function() {
+        if ( !hasFinish ) {
+            if ( !isCloseWifi ) {
+                flag = true;
+                img.src = 'X:\\';
+                console.log('Ping ' + ip + ' success. ');
+            } else {
+                console.log('network is not working!');
+            }
+            hasFinish = true;
+        }
+    };
+
     setTimeout(function(){
         isCloseWifi = false;
         console.log('network is working, start ping...');
     },2);
-    img.src = "http://" + ip + "/" + start;
+
+    img.src = 'http://' + ip + '/' + start;
     var timer = setTimeout(function() {
         if ( !flag ) {
+            hasFinish = true;
+            img.src = 'X://';
             flag = false ;
             console.log('Ping ' + ip + ' fail. ');
         }
